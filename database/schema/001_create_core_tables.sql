83ec5fc56a6e1e0a3635e94685f71fc765069ad8
Daniel <Nevaslon0117@gmail.com>
2026-07-22 09:02:51 -0700

    added constraints to accounts and categories tables

--- database/schema/001_create_core_tables.sql
+++ database/schema/001_create_core_tables.sql
@@ -1,31 +1,65 @@
 CREATE TABLE accounts (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    account_name VARCHAR(150) UNIQUE NOT NULL,
+    account_name VARCHAR(150) NOT NULL,
     account_type VARCHAR(150) NOT NULL,
     institution_name VARCHAR(150),
-    current_balance NUMERIC(12, 2) NOT NULL,
-    credit_limit NUMERIC(12, 2),
+    current_balance NUMERIC(12,2) NOT NULL,
+    credit_limit NUMERIC(12,2),
     opened_date DATE,
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-    is_active BOOLEAN NOT NULL DEFAULT TRUE 
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+
+    CONSTRAINT chk_accounts_account_type
+        CHECK (
+            account_type IN (
+                'checking',
+                'savings',
+                'credit',
+                'cash',
+                'investment'
+            )
+        ),
+
+    CONSTRAINT chk_accounts_current_balance
+        CHECK (
+            current_balance >= 0
+        ),
+
+    CONSTRAINT chk_accounts_credit_limit
+        CHECK (
+            credit_limit IS NULL
+            OR credit_limit >= 0
+        )
 );
+
 
 CREATE TABLE categories (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     category_name VARCHAR(150) UNIQUE NOT NULL,
     category_type VARCHAR(150) NOT NULL,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
-    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
+    created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+
+    CONSTRAINT chk_categories_category_type
+        CHECK (
+            category_type IN (
+                'income',
+                'expense',
+                'transfer'
+            )
+        )
 );
+
 
 CREATE TABLE merchants (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     merchant_name VARCHAR(150) UNIQUE NOT NULL,
     normalized_name VARCHAR(150) UNIQUE NOT NULL,
     merchant_type VARCHAR(150) NOT NULL,
-    is_active BOOLEAN NOT NULL DEFAULT TRUE
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
+
 
 CREATE TABLE transactions (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -36,33 +70,36 @@ CREATE TABLE transactions (
     source VARCHAR(150) NOT NULL,
     notes TEXT,
     posted_date DATE,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     merchant_id INT REFERENCES merchants(id),
     category_id INT REFERENCES categories(id),
     account_id INT NOT NULL REFERENCES accounts(id)
 );
+
 
 CREATE TABLE debts (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     debt_name VARCHAR(150) NOT NULL,
     lender_name VARCHAR(150) NOT NULL,
     status VARCHAR(50) NOT NULL,
-    current_balance NUMERIC(12,2)NOT NULL,
-    original_balance NUMERIC(12,2)NOT NULL,
-    minimum_payment NUMERIC(12,2)NOT NULL,
+    current_balance NUMERIC(5,2) NOT NULL,
+    original_balance NUMERIC(5,2)
+        CHECK (original_balance >= 0),
+    minimum_payment NUMERIC(5,2)
+        CHECK (minimum_payment >= 0),
     interest_rate DECIMAL(5,2) NOT NULL,
     due_day INT NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
     account_id INT REFERENCES accounts(id)
 );
 
+
 CREATE TABLE savings_goals (
     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     goal_name VARCHAR(150) UNIQUE NOT NULL,
-    target_amount NUMERIC(12,2)NOT NULL,
-    current_amount NUMERIC(12,2)NOT NULL,
+    target_amount NUMERIC(5,2) NOT NULL,
+    current_amount NUMERIC(5,2) NOT NULL,
     status VARCHAR(150) NOT NULL,
     target_date DATE,
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
-
 );
